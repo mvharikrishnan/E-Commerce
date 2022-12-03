b6823d5478f82c810a44a5193b103353966f0896
mvharikrishnan <harikrish.hmv@gmail.com>
2022-12-03 10:32:13 +0530

    Creator Started

---
+++ lib/view/presentation/creator/home/creator_home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Creator_Home_screen extends StatelessWidget {
+  const Creator_Home_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
