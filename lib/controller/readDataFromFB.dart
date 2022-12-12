da7d7e465eef8a7a331fda3a82c137c1a48a2b42
mvharikrishnan <harikrish.hmv@gmail.com>
2022-12-12 19:31:15 +0530

    Product screen updated without image

--- lib/controller/readDataFromFB.dart
+++ lib/controller/readDataFromFB.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerceapp/model/ProductModel/productModel.dart';
 
 Stream<List<ProductModel>> FetchProducts({required String CollectionName}) =>
-    FirebaseFirestore.instance.collection('CreatorsProducts').snapshots().map(
+    FirebaseFirestore.instance.collection(CollectionName).snapshots().map(
         (snapShot) => snapShot.docs
             .map((doc) => ProductModel.fromJson(doc.data()))
             .toList());
