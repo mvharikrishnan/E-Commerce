1d31e1abd55ffabae8fa39ccf02fc5fa8d48e51a
mvharikrishnan <harikrish.hmv@gmail.com>
2022-12-30 13:14:21 +0530

    Creators active products displayed

--- lib/view/presentation/creator/active_product/active_products_screen.dart
+++ lib/view/presentation/creator/active_product/active_products_screen.dart
@@ -1,4 +1,7 @@
+import 'package:ecommerceapp/controller/readDataFromFB.dart';
 import 'package:ecommerceapp/core/constants/user/constants.dart';
+import 'package:ecommerceapp/model/ProductModel/productModel.dart';
+import 'package:ecommerceapp/view/presentation/creator/active_product/widgets/activeProductTile.dart';
 import 'package:ecommerceapp/view/presentation/creator/widgets/appBarCreator.dart';
 import 'package:ecommerceapp/view/presentation/creator/widgets/back_ground_color.dart';
 import 'package:ecommerceapp/view/widget/divider.dart';
@@ -16,171 +19,63 @@ class Active_Products_screen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: SafeArea(
-          child: Column(
-            children: [
-              appBarCreator(context),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Active Products',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    sizedBoxHeight10,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              width: 112,
-                              height: 126,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: AssetImage(
-                                          'assets/drawingImage/drawing2.jpeg'))),
-                            ),
-                            sizedBox10,
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  'Pencil Portrait A4',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  'Smiling Girl Portrail on 300 GSM Canvas \ndrawin with Steadler Pencil\n. Strathmore A4 Paper.',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal),
-                                ),
-                                Text(
-                                  '₹150',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                        sizedBoxHeight10,
-                        Container(
-                          height: 40,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.amber),
-                          child: const Center(
-                              child: Text(
-                            'Edit Product',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          )),
-                        ),
-                        sizedBoxHeight10,
-                        Container(
-                          height: 40,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Color(0xFFFC7F0B)),
-                          child: const Center(
-                              child: Text(
-                            'Remove',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          )),
-                        ),
-                      ],
-                    ),
-                    sizedBoxHeight10,
-                    DividerEcommerce(),
-                    //?delete it after ui Presentation
-                    sizedBoxHeight10,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              width: 112,
-                              height: 126,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: AssetImage(
-                                          'assets/drawingImage/drawing2.jpeg'))),
-                            ),
-                            sizedBox10,
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  'Pencil Portrait A4',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  'Smiling Girl Portrail on 300 GSM Canvas \ndrawin with Steadler Pencil\n. Strathmore A4 Paper.',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.normal),
-                                ),
-                                Text(
-                                  '₹150',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                        sizedBoxHeight10,
-                        Container(
-                          height: 40,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.amber),
-                          child: const Center(
-                              child: Text(
-                            'Edit Product',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          )),
-                        ),
-                        sizedBoxHeight10,
-                        Container(
-                          height: 40,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Color(0xFFFC7F0B)),
-                          child: const Center(
-                              child: Text(
-                            'Remove',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          )),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                appBarCreator(context),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Active Products',
+                        style:
+                            TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      sizedBoxHeight10,
+                      StreamBuilder<List<ProductModel>>(
+                        stream: FetchProducts(CollectionName: 'CreatorsProducts'),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
+                          if (snapshot.hasError) {
+                            return const Center(
+                              child: Text('Some Error Occurs'),
+                            );
+                          }
+                          if (snapshot.hasData) {
+                            final creatorProducts = snapshot.data!;
+          
+                            return ListView(
+                                shrinkWrap: true,
+                                physics: ScrollPhysics(),
+                                children: creatorProducts
+                                    .map(BuildCreatorProducts)
+                                    .toList());
+                          } else {
+                            return const Center(
+                              child: Text('NO PRODUCTS FOUND PLEASE ADD'),
+                            );
+                          }
+                        },
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+Widget BuildCreatorProducts(ProductModel productModel) => activeProductTile(
+      productModel: productModel,
+    );
---
+++ lib/view/presentation/creator/active_product/widgets/activeProductTile.dart
@@ -0,0 +1,81 @@
+import 'package:ecommerceapp/core/constants/user/constants.dart';
+import 'package:ecommerceapp/model/ProductModel/productModel.dart';
+import 'package:ecommerceapp/view/widget/divider.dart';
+
+import 'package:flutter/material.dart';
+
+class activeProductTile extends StatelessWidget {
+  const activeProductTile({required this.productModel, super.key});
+  final ProductModel productModel;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            sizedBoxHeight10,
+            Container(
+              width: 112,
+              height: 126,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(productModel.productImage))),
+            ),
+            sizedBox10,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  productModel.productName,
+                  
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                sizedBoxHeight10,
+                Text(
+                  productModel.category,
+                  overflow: TextOverflow.ellipsis,
+                   softWrap: true,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+                ),
+                sizedBoxHeight10,
+                Text(
+                  "₹${productModel.productPrice}",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
+            )
+          ],
+        ),
+        sizedBoxHeight10,
+        Container(
+          height: 40,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10), color: Colors.amber),
+          child: const Center(
+              child: Text(
+            'Edit Product',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          )),
+        ),
+        sizedBoxHeight10,
+        Container(
+          height: 40,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Color(0xFFFC7F0B)),
+          child: const Center(
+              child: Text(
+            'Remove',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          )),
+        ),
+        sizedBoxHeight10,
+        DividerEcommerce(),
+      ],
+    );
+  }
+}
